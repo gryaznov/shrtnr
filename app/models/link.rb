56ed307f47aedf8cff072feea9a5e64ba12ff71d
Stanislav Gryaznov <redzagogulin@gmail.com>
2017-11-23 18:49:31 +0200

    Implement Link model.

---
+++ app/models/link.rb
@@ -0,0 +1,21 @@
+class Link < ApplicationRecord
+  URL_REGEXP = %r{(http|https|www)(:\/\/|\.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?}
+
+  attr_readonly :shorten
+
+  validates :original, presence: true, format: { with: URL_REGEXP }
+  validates :shorten, uniqueness: true, allow_blank: true
+
+  before_validation :set_shorten, on: :create
+
+  def set_shorten
+    self.shorten = Link.generate_shorten
+  end
+
+  def self.generate_shorten
+    loop do
+      suggestion = "#{Settings.host}/#{SecureRandom.hex(3)}"
+      break suggestion unless exists?(shorten: suggestion)
+    end
+  end
+end
